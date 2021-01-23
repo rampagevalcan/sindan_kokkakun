4124ce15c9c53a645f40ca190efc5fa30415d050
rampagevalcan <takeofftowardadream1019@gmail.com>
2021-01-23 15:48:39 +0900

    診断結果ページに写真追加

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -27,16 +27,16 @@ function countAnswer(){
       }
     }
     if(count_a>count_b&&count_a>count_c){
-    alert(`Aを${count_a}個 Bを${count_b}個 Cを${count_c}個チェックしました
+    alert(`Aを${count_a}個 Bを${count_b}個 Cを${count_c}個チェックしました。
 Aが多かったので、ぺージAに進んでください。`);
     }else if(count_b>count_a&&count_b>count_c){
-    alert(`Aを${count_a}個 Bを${count_b}個 Cを${count_c}個チェックしました
+    alert(`Aを${count_a}個 Bを${count_b}個 Cを${count_c}個チェックしました。
 Bが多かったので、ぺージBに進んでください。`);
     }else if(count_c>count_a&&count_c>count_b){
-    alert(`Aを${count_a}個 Bを${count_b}個 Cを${count_c}個チェックしました
+    alert(`Aを${count_a}個 Bを${count_b}個 Cを${count_c}個チェックしました。
 Cが多かったので、ぺージCに進んでください。`);
     }else{
-      alert(`Aを${count_a}個 Bを${count_b}個 Cを${count_c}個チェックしました
+      alert(`Aを${count_a}個 Bを${count_b}個 Cを${count_c}個チェックしました。
 あなたはミックスタイプです。`);
     }
  };
